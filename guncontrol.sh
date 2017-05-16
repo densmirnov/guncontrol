01774391df30e72df3856cc495f77e43662f9be7
Denis Smirnov <densmirnov@me.com>
2017-05-17 02:23:58 +0300

    Further improvements

--- guncontrol.sh
+++ guncontrol.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-##  GUNBOT 3.2 CUSTOM LAUNCHER
-##  version v0.0.1 / May 2017.
+##  GUNCONTROL • GUNBOT 3.2 CUSTOM LAUNCHER
+##  version v0.0.2 / May 2017.
 ##
 ##
 ##  Denis Smirnov / densmirnov@me.com / densmirnov.com
+##  Also credits goes to gionni@gunthy.org for his GunBot Start/Stop Bash Script
 ##
 ##  BTC: 1denG4FjcXDeGSYeiv65R6Eyi3HWo7R8j
 ##  ETH: 0xa4C7fACEFC08e684Cd9043c3e31C86Dfb88DF75a
@@ -22,7 +23,7 @@ YELLOW=$(tput setaf 3)
 WHITE=$(tput setaf 7)
 RESET=$(tput sgr0)
 TOPLINE=""
-BOTTOMLINE="\n────────────────────────────────────────────────────────────────"
+BOTTOMLINE="\n ───────────────────────────────────────────────────────────── ─"
 EMPTYLINE="│                                                              │"
 
 export CMD && export BOTFOLDER && export FILES && export NAME
@@ -30,8 +31,8 @@ CMD=$1
 BOTFOLDER="${PWD}"
 FILES="*-config.js"
 
-# clear
-echo -e "${BLUE}""┌──────────────────────────────────────────────────────────────┐\n${BLUE}│${RESET}  ${GREEN}${BOLD}GUNBOT LAUNCHER v0.0.2${RESET}                   ${BLUE} $(date +%d.%m.%Y\ %H:%M)${RESET}  ${BLUE}│\n└──────────────────────────────────────────────────────────────┘\n""${RESET}"
+clear
+echo -e "${BLUE}""\n┌───────────────────────────────────────────────────────┐\n${BLUE}│${RESET}  ${GREEN}GUNBOT LAUNCHER v0.0.2${RESET}             ${BLUE}$(date +%d.%m.%Y\ %H:%M)${RESET}  ${BLUE}│\n└───────────────────────────────────────────────────────┘\n""${RESET}"
 
 ##  OPTIONS
 ##  -----------------------------------------------
@@ -40,50 +41,46 @@ if [[ -n "$CMD" ]]; then
   ##  START
   ##  -----------------------------------------------
   if [[ "$CMD" == "start" ]]; then
+    echo -e "${WHITE}""   STARTING GUNBOT...\n ─────────────────────────────────────────────────────── " "${RESET}"
     for f in $FILES
     do
-      NAME="${f%-*}"
-      echo -e "${WHITE}""• Checking if ${YELLOW}$NAME ${WHITE}is running...${RESET}\t\c"
-      if ! screen -list | grep -qw "$NAME"; then
-        echo -e "${RED}【${BOLD}NO!${RESET}】${RESET}\t\c"
-        echo -n "${BLUE} [ starting ${RESET}"
+      NAME1="${f%-*}"
+      NAME=${NAME1#"poloniex-"}
+
+      echo -e "${WHITE}"" • Checking ${YELLOW}$NAME${WHITE}...  ${RESET}\c"
+      if ! screen -list | grep -q "$NAME"; then
+        echo -e "${BLUE}""\t\t\tSTOPPED!\n   Starting \c""${RESET}"
         screen -dmS "$NAME" "${BOTFOLDER}"/gunbot "$NAME" poloniex && sleep 1
         echo -n "${BLUE}...1 ${RESET}" && sleep 1
         echo -n "${BLUE}...2 ${RESET}" && sleep 1
         echo -n "${BLUE}...3 ${RESET}" && sleep 1
         echo -n "${BLUE}...4 ${RESET}" && sleep 1
-        echo -e "${BLUE}...5! ]  ${RESET}${GREEN}${BOLD}+ Done!${RESET}\n"
+        echo -e "${BLUE}...5!  ${RESET}${GREEN}            DONE!${RESET}"
       else
-        echo -e "\t${GREEN}【${BOLD}YES!】${RESET}\t\c"
-        echo -n "${BLUE} [ skipping... ]${RESET}"
-        echo -e " " && sleep 0.2
+        echo -e "${BLUE}""\t\t\tRUNNING!\n   Skipping...                                     ${GREEN}DONE!""${RESET}" && sleep 0.2
       fi
     done
-    echo -e " "
-    echo -e "${GREEN}""${BOLD}""  Done!${RESET} ${GREEN}All Pairs are running!\n""${RESET}"
+    echo -e "${WHITE}"" ─────────────────────────────────────────────────────── \n   ALL PAIRS ARE RUNNING!" "${RESET}"
     exit 0
   fi
 
   ##  STOP
   ##  -----------------------------------------------
   if [[ "$CMD" == "stop" ]]; then
+    echo -e "${WHITE}""   STOPPING GUNBOT...\n ─────────────────────────────────────────────────────── \n" "${RESET}"
     for f in $FILES
     do
-      NAME="${f%-*}"
-      echo -e "${WHITE}• Checking if ${YELLOW}$NAME ${WHITE}is running...\t${RESET}\c"
+      NAME1="${f%-*}"
+      NAME=${NAME1#"poloniex-"}
+      echo -e "${WHITE} • Checking ${YELLOW}$NAME ${WHITE}... \t\t\t${RESET}\c"
       if ! screen -list | grep -q "$NAME"; then
-        echo -e "${GREEN}【${BOLD}NO!${RESET}】${RESET}\t\c"
-        echo -n "${BLUE} [ skipping... ]${RESET}"
-        echo -e " " && sleep 0.2
+        echo -e "${BLUE}""STOPPED!\n   Skipping...                                     ${GREEN}DONE!""${RESET}" && sleep 0.2
       else
-        echo -e "${RED}【${BOLD}YES!${RESET}】${RESET}\t\c"
-        echo -n "${BLUE}[ exiting... ]${RESET}"
-        echo -e " " && sleep 0.2
+        echo -e "${BLUE}""RUNNING!\n   Stopping...                                     ${GREEN}DONE!""${RESET}" && sleep 0.2
         screen -S "$NAME" -X quit && sleep 0.2
       fi
     done
-    echo -e " "
-    echo -e "  ${GREEN}${BOLD}DONE!${RESET} ${GREEN}All Pairs are stopped! ${RESET}\n"
+    echo -e "${WHITE}"" ─────────────────────────────────────────────────────── \n   ALL PAIRS ARE STOPPED!" "${RESET}"
     exit 0
   fi
 
@@ -93,18 +90,18 @@ if [[ -n "$CMD" ]]; then
     export LOOKUP && export HEADER && export DIVIDER
     LOOKUP="ps aux"
     HEADER="%-12b\t%-12s\t%-12b\n"
-    DIVIDER=${BLUE}"------------"${RESET}
-    echo -e "${WHITE}""  RUNNING PAIRS:" "${RESET}"
-    echo -e " ${WHITE}──────────────────────────────────────────────────────────── ""${RESET}"
+    DIVIDER=${BLUE}"   ------------"${RESET}
+    echo -e "${WHITE}""   RUNNING PAIRS:\n ─────────────────────────────────────────────────────── \n" "${RESET}"
+    # echo -e "${BLUE}""   STRATEGY\t" "  PAIR\t" "  EXCHANGE\t""${RESET}"
     # echo -e "${DIVIDER}" "${DIVIDER}" "${DIVIDER}"
-    $LOOKUP | grep -v grep | grep "SCREEN" | awk '{printf "   %-12s\t   %-12s\t   %-12s  │ \n", $09, $13, $16}'
-    # echo -e "${BLUE}""${BOTTOMLINE}""${RESET}"
+    # $LOOKUP | grep -v grep | grep "SCREEN" | awk '{printf "   %-12s\t   %-12s\t   %-12s   \n", $09, $13, $16}'
+    $LOOKUP | grep -v grep | grep "SCREEN" | awk '{printf " • %-09s\n", $13}'
+    echo -e "${WHITE}""\n ─────────────────────────────────────────────────────── " "${RESET}"
     exit 0
   fi
 else
-  echo -e "${RED}""  ${RED}COMMAND MISSING!""${RESET}"
-  echo -e " ${RED}─────────────────────────────────────────────────────────────""${RESET}"
+  echo -e "${RED}""  ${RED}COMMAND MISSING\n ─────────────────────────────────────────────────────── \n""${RESET}"
   echo -e "  ${BLUE}Usage: ${WHITE}bash guncontrol.sh [${GREEN}${BOLD} start ${RESET}| ${RED}${BOLD}stop ${RESET}| ${YELLOW}${BOLD}list${RESET} ]${RESET}"
-  # echo -e "${BLUE}""${BOTTOMLINE}""${RESET}"
+    echo -e "${RED}""\n ─────────────────────────────────────────────────────── " "${RESET}"
   exit 0
 fi
