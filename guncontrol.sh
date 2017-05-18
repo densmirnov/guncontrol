ae4aeb05181dc017670200e6fa241fd1931d72e0
Denis Smirnov <densmirnov@me.com>
2017-05-18 13:05:33 +0300

    Added OS Detection

--- guncontrol.sh
+++ guncontrol.sh
@@ -14,94 +14,88 @@
 
 ##  STARTUP ITEMS
 ##  -----------------------------------------------
-export BOLD && export RED && export GREEN && export BLUE && export YELLOW && export WHITE && export RESET && export TOPLINE && export BOTTOMLINE && export EMPTYLINE
-BOLD=$(tput bold)
-RED=$(tput setaf 1)
-GREEN=$(tput setaf 2)
-BLUE=$(tput setaf 4)
-YELLOW=$(tput setaf 3)
-WHITE=$(tput setaf 7)
-RESET=$(tput sgr0)
-TOPLINE=""
-BOTTOMLINE="\n ───────────────────────────────────────────────────────────── ─"
-EMPTYLINE="│                                                              │"
+    export RESET && export BOLD && export RED && export GREEN && export BLUE && export YELLOW && export WHITE && export BOTTOM
+    RESET=$(tput sgr0) && BOLD=$(tput bold) && RED=$(tput setaf 1) && GREEN=$(tput setaf 2) && BLUE=$(tput setaf 4) && YELLOW=$(tput setaf 3) && WHITE=$(tput setaf 7) && BOTTOM="\n ─────────────────────────────────────────────────────── \n"
 
-export CMD && export BOTFOLDER && export FILES && export NAME
-CMD=$1
-BOTFOLDER="${PWD}"
-FILES="*-config.js"
+    export CMD && export ARCH && export BOTFOLDER && export FILES && export NAME && export NAME1 && export GUNTHY
+    CMD=$1 && ARCH=$(uname -m) && BOTFOLDER="$(pwd)" && FILES="*-config.js" && NAME1="${f%-*}" && NAME=${NAME1#"poloniex-"} && GUNTHY=""
 
-clear
-echo -e "${BLUE}""\n┌───────────────────────────────────────────────────────┐\n${BLUE}│${RESET}  ${GREEN}GUNBOT LAUNCHER v0.0.2${RESET}             ${BLUE}$(date +%d.%m.%Y\ %H:%M)${RESET}  ${BLUE}│\n└───────────────────────────────────────────────────────┘\n""${RESET}"
-
-##  OPTIONS
+##  OS DETECTION
 ##  -----------------------------------------------
-if [[ -n "$CMD" ]]; then
+    if [[ "$OSTYPE" == "linux-gnu" && "$ARCH" == "x86_64" ]]; then
+      GUNTHY="gunthy-linuxx64"
+    elif [[ "$OSTYPE" == "linux-gnu" && "$ARCH" == "i686" ]]; then
+      GUNTHY="gunthy-linuxx86"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+      GUNTHY="gunthy-macos"
+    else
+      GUNTHY="Unknown OS"
+    fi
 
-  ##  START
-  ##  -----------------------------------------------
-  if [[ "$CMD" == "start" ]]; then
-    echo -e "${WHITE}""   STARTING GUNBOT...\n ─────────────────────────────────────────────────────── " "${RESET}"
-    for f in $FILES
-    do
-      NAME1="${f%-*}"
-      NAME=${NAME1#"poloniex-"}
 
-      echo -e "${WHITE}"" • Checking ${YELLOW}$NAME${WHITE}...  ${RESET}\c"
-      if ! screen -list | grep -wo "$NAME"; then
-        echo -e "${BLUE}""\t\t\tSTOPPED!\n   Starting \c""${RESET}"
-        screen -dmS "$NAME" "${BOTFOLDER}"/gunbot "$NAME" poloniex && sleep 1
-        echo -n "${BLUE}...1 ${RESET}" && sleep 1
-        echo -n "${BLUE}...2 ${RESET}" && sleep 1
-        echo -n "${BLUE}...3 ${RESET}" && sleep 1
-        echo -n "${BLUE}...4 ${RESET}" && sleep 1
-        echo -e "${BLUE}...5!  ${RESET}${GREEN}            DONE!${RESET}"
-      else
-        echo -e "${BLUE}""\t\t\tRUNNING!\n   Skipping...                                     ${GREEN}DONE!""${RESET}" && sleep 0.2
+##  MAIN SCRIPT
+##  -----------------------------------------------
+    # clear
+    echo -e "${BLUE}""\n┌───────────────────────────────────────────────────────┐\n${BLUE}│${RESET}  ${GREEN}GUNBOT LAUNCHER v0.0.2${RESET}             ${BLUE}$(date +%d.%m.%Y\ %H:%M)${RESET}  ${BLUE}│\n└───────────────────────────────────────────────────────┘""${RESET}"
+
+    if [[ -n "$CMD" ]]; then
+      ##
+      ##  START
+      ##  -----------------------------------------------
+      if [[ "$CMD" == "start" ]]; then
+        echo -e "${WHITE}""   STARTING GUNBOT...$BOTTOM" "${RESET}"
+        for f in $FILES
+        do
+          echo -e "${WHITE}"" • Checking ${YELLOW}$NAME${WHITE}...  ${RESET}\c"
+          if ! screen -list | grep -wo "$NAME"; then
+            echo -e "${BLUE}""\t\t\tSTOPPED!\n   Starting \c""${RESET}"
+            screen -dmS "$NAME" "${BOTFOLDER}"/"$GUNTHY" "$NAME" poloniex && sleep 1
+            echo -n "${BLUE}...1 ${RESET}" && sleep 1
+            echo -n "${BLUE}...2 ${RESET}" && sleep 1
+            echo -n "${BLUE}...3 ${RESET}" && sleep 1
+            echo -n "${BLUE}...4 ${RESET}" && sleep 1
+            echo -e "${BLUE}...5!  ${RESET}${GREEN}            DONE!${RESET}"
+          else
+            echo -e "${BLUE}""\t\t\tRUNNING!\n   Skipping...                                     ${GREEN}DONE!""${RESET}" && sleep 0.2
+          fi
+        done
+        echo -e "${WHITE}""\n ─────────────────────────────────────────────────────── \n   ALL PAIRS ARE RUNNING!" "${RESET}"
+        exit 0
       fi
-    done
-    echo -e "${WHITE}"" ─────────────────────────────────────────────────────── \n   ALL PAIRS ARE RUNNING!" "${RESET}"
-    exit 0
-  fi
 
-  ##  STOP
-  ##  -----------------------------------------------
-  if [[ "$CMD" == "stop" ]]; then
-    echo -e "${WHITE}""   STOPPING GUNBOT...\n ─────────────────────────────────────────────────────── \n" "${RESET}"
-    for f in $FILES
-    do
-      NAME1="${f%-*}"
-      NAME=${NAME1#"poloniex-"}
-      echo -e "${WHITE} • Checking ${YELLOW}$NAME ${WHITE}... \t\t\t${RESET}\c"
-      if ! screen -list | grep -q "$NAME"; then
-        echo -e "${BLUE}""STOPPED!\n   Skipping...                                     ${GREEN}DONE!""${RESET}" && sleep 0.2
-      else
-        echo -e "${BLUE}""RUNNING!\n   Stopping...                                     ${GREEN}DONE!""${RESET}" && sleep 0.2
-        screen -S "$NAME" -X quit && sleep 0.2
+      ##  STOP
+      ##  -----------------------------------------------
+      if [[ "$CMD" == "stop" ]]; then
+        echo -e "${WHITE}""   STOPPING GUNBOT...$BOTTOM" "${RESET}"
+        for f in $FILES
+        do
+          echo -e "${WHITE} • Checking ${YELLOW}$NAME ${WHITE}... \t\t\t${RESET}\c"
+          if ! screen -list | grep -q "$NAME"; then
+            echo -e "${BLUE}""STOPPED!\n   Skipping...                                     ${GREEN}DONE!""${RESET}" && sleep 0.2
+          else
+            echo -e "${BLUE}""RUNNING!\n   Stopping...                                     ${GREEN}DONE!""${RESET}" && sleep 0.2
+            screen -S "$NAME" -X quit && sleep 0.2
+          fi
+        done
+        echo -e "${WHITE}""$BOTTOM   ALL PAIRS ARE STOPPED!" "${RESET}"
+        exit 0
       fi
-    done
-    echo -e "${WHITE}"" ─────────────────────────────────────────────────────── \n   ALL PAIRS ARE STOPPED!" "${RESET}"
-    exit 0
-  fi
 
-  ##  LIST
-  ##  -----------------------------------------------
-  if [[ "$CMD" == "list" ]]; then
-    export LOOKUP && export HEADER && export DIVIDER
-    LOOKUP="ps aux"
-    HEADER="%-12b\t%-12s\t%-12b\n"
-    DIVIDER=${BLUE}"   ------------"${RESET}
-    echo -e "${WHITE}""   RUNNING PAIRS:\n ─────────────────────────────────────────────────────── \n" "${RESET}"
-    # echo -e "${BLUE}""   STRATEGY\t" "  PAIR\t" "  EXCHANGE\t""${RESET}"
-    # echo -e "${DIVIDER}" "${DIVIDER}" "${DIVIDER}"
-    # $LOOKUP | grep -v grep | grep "SCREEN" | awk '{printf "   %-12s\t   %-12s\t   %-12s   \n", $09, $13, $16}'
-    $LOOKUP | grep -v grep | grep "SCREEN" | awk '{printf " • %-09s\n", $13}'
-    echo -e "${WHITE}""\n ─────────────────────────────────────────────────────── " "${RESET}"
-    exit 0
-  fi
-else
-  echo -e "${RED}""  ${RED}COMMAND MISSING\n ─────────────────────────────────────────────────────── \n""${RESET}"
-  echo -e "  ${BLUE}Usage: ${WHITE}bash guncontrol.sh [${GREEN}${BOLD} start ${RESET}| ${RED}${BOLD}stop ${RESET}| ${YELLOW}${BOLD}list${RESET} ]${RESET}"
-    echo -e "${RED}""\n ─────────────────────────────────────────────────────── " "${RESET}"
-  exit 0
-fi
+      ##  LIST
+      ##  -----------------------------------------------
+      if [[ "$CMD" == "list" ]]; then
+        export LOOKUP && export HEADER && export DIVIDER
+        LOOKUP="ps aux"
+        HEADER="%-12b\t%-12s\t%-12b\n"
+        DIVIDER=${BLUE}"   ------------"${RESET}
+        echo -e "${WHITE}""   RUNNING PAIRS:$BOTTOM" "${RESET}"
+        $LOOKUP | grep -v grep | grep "SCREEN" | awk '{printf " • %-09s\n", $13}'
+        echo -e "${WHITE}""$BOTTOM" "${RESET}"
+        exit 0
+      fi
+    else
+      echo -e "${RED}""  ${RED}COMMAND MISSING$BOTTOM""${RESET}"
+      echo -e "  ${BLUE}Usage: ${WHITE}bash guncontrol.sh [${GREEN}${BOLD} start ${RESET}| ${RED}${BOLD}stop ${RESET}| ${YELLOW}${BOLD}list${RESET} ]${RESET}"
+        echo -e "${RED}""$BOTTOM" "${RESET}"
+      exit 0
+    fi
